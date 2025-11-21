af63cfe42fd8f658f2e5667c463f778ec82db154
jwchoi <jongwon.c@fastfive.co.kr>
2025-11-21 15:20:40 +0900

    chore: domain module

---
+++ domain/src/main/kotlin/com/example/domain/DomainService.kt
@@ -0,0 +1,10 @@
+package com.example.domain
+
+import org.springframework.stereotype.Service
+
+@Service
+class DomainService {
+    fun getDomainData(): String {
+        return "Domain Data"
+    }
+}
